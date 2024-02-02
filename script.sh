cb8e3a7f317f840728dc368b6f748084631ec41e
Sheryna1 <75541035+Sheryna1@users.noreply.github.com>
2024-02-02 19:03:47 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 while true; do
     echo "heny bithes"
-    npx ts-node send_universal.ts --givers $1 --gpu $2 --seed "$3"
+    npx ts-node send_universal.ts --givers $1 --gpu $2 --seed "$3" --api $4
 done
