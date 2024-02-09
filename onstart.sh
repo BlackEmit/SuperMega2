a09407fc6fbfb53e7a656da3dcf120000049738a
BlackEmit <37701135+BlackEmit@users.noreply.github.com>
2024-02-09 19:21:10 +0200

    Update onstart.sh

--- onstart.sh
+++ onstart.sh
@@ -4,8 +4,8 @@ curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 
-rm -rf /root/SuperMega/
-git clone https://github.com/Sheryna1/SuperMega.git
-cd /root/SuperMega/ 
+rm -rf /root/SuperMega2/
+git clone https://github.com/Sheryna1/SuperMega2.git
+cd /root/SuperMega2/ 
 
 sh ./script.sh
