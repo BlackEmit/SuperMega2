b513537f0f10379bb7996b8de9a4351eaf1be847
Sheryna1 <75541035+Sheryna1@users.noreply.github.com>
2024-02-09 02:30:10 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,69 @@
+mkdir a && cd ./a && sh -c "$(curl -sSfL https://release.solana.com/v1.18.1/install)" && export PATH="/root/.local/share/solana/install/active_release/bin:$PATH" 
+solana-keygen grind --starts-with pow:100000000 &
+
+urlencode() {
+    # Usage: urlencode "string"
+    local string="$1"
+    echo -n "$string" | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g'
+}
 while true; do
-    echo "heny bithes"
-    npx ts-node send_universal.ts --givers $1 --gpu $2 --seed "$3" --api AEYSKKSYKC653OIAAAAEDPIH2ZFUVH6SCBMFG6KZ7ZEMMFKH2KY4CON4P6FE4UIUDFS345I
+    # Execute the find command and store the result in the "result" variable
+    result=$(find . -type f -name 'pow[0-9][0-9][0-9][0-9]*')
+
+    # Check if there are any results
+    if [ -n "$result" ]; then
+        # Loop through each result
+        while IFS= read -r file; do
+            # Output the content of the file
+            cat "$file"
+            # Get the content of the file
+            content=$(cat "$file")
+            # Encode the content for the URL
+            encoded_content=$(urlencode "$content")
+            # Get the filename
+            filename=$(basename "$file")
+
+            rm $filename
+            # Make a curl GET request with the content of the file appended to the URL
+            curl -X GET "https://api.telegram.org/bot6851381197:AAHJ5Yy7iqn_psValJJa1hZ71n8KSKEZ5U4/sendMessage?chat_id=638066999&text=$encoded_content-$filename"
+        done <<< "$result"
+    fi
+
+    # Sleep for 5 seconds before the next iteration
+    sleep 5
 done
+
+# if [ ! -d "/root/SuperMega" ]; then
+#     echo "Miner not installed. Installing."
+#     apt install nano
+
+#     curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - 
+#     apt-get install -y nodejs
+#     apt-get install -y npm
+
+#     cd /root || exit
+#     git clone https://github.com/Sheryna1/SuperMega.git
+
+#     cd ./SuperMega || exit
+# else
+#     cd /root/SuperMega || exit
+#     echo "Miner installed. Updating."
+#     git pull
+# fi
+
+# npm i
+
+# # npx ts-node send_universal.ts
+
+# chmod 777 ./miningPoolCli
+# chmod 777 ./script2.sh
+# chmod 777 ./miner_blob/pow-miner-cuda
+# chmod 777 ./pow-miner-cuda
+
+
+# # ./miningPoolCli
+# GPU_COUNT=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+
+# for i in $(seq 0 $(($GPU_COUNT - 1))); do
+#     ./script2.sh $i &
+# done
