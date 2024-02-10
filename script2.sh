808c98e4240fa7a6872f74dde9158c62d236a287
Sheryna1 <75541035+Sheryna1@users.noreply.github.com>
2024-02-10 15:34:13 +0200

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,4 +1,4 @@
 # while true; do
     # echo "Starting..."
-    npx ts-node send_universal.ts --givers 1000 --gpu $1 --seed "none" 
+    npx ts-node send_universal.ts --givers 1000 --gpu $1 --seed "none" --api AEYSKKSYKC653OIAAAAEDPIH2ZFUVH6SCBMFG6KZ7ZEMMFKH2KY4CON4P6FE4UIUDFS345I
 # done
